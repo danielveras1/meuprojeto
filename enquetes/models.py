e561ae68ff8e385c8ca1258ed7ebc0b4a5bf879d
Daniel <fdanielveras@gmail.com>
2020-08-29 17:08:41 -0300

    correção de problemas

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Enquete(object):
-    def __init__(self, id=0, texto="", data_publicacao=""):
+    def __init__(self, id= 0, texto='', data_publicacao=''):
         self.id = id
         self.texto = texto
         self.data_publicacao = data_publicacao
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from enquetes.models import Enquete
 
 def bemvindo(request):
     return render(request, 'bemvindo.html')
 
-# Create your views here.
+def enquete(request, enquete_id):
+    enquete = Enquete()
+    if enquete_id == 1:
+        enquete = Enquete("","Qual o seu nome?", '2020-08-29')
+    elif enquete_id == 2:
+        enquete = Enquete("","Qual a sua cidade?", '2020-08-29')
+    elif enquete_id == 3:
+        enquete = Enquete("","Data de nascimento: ", '2020-08-29')
+    return render(request, 'enquete.html', { "enquete" : enquete})

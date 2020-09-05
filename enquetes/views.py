f22862dc2743699a89717664550f86fa8a0b7f3c
Daniel <fdanielveras@gmail.com>
2020-09-05 13:36:00 -0300

    Views.py foi editada

--- enquetes/views.py
+++ enquetes/views.py
@@ -4,12 +4,7 @@ from enquetes.models import Enquete
 def bemvindo(request):
     return render(request, 'bemvindo.html')
 
-def enquete(request, enquete_id):
+def enquete(request,enquete_id):
     enquete = Enquete()
-    if enquete_id == 1:
-        enquete = Enquete("","Qual o seu nome?", '2020-08-29')
-    elif enquete_id == 2:
-        enquete = Enquete("","Qual a sua cidade?", '2020-08-29')
-    elif enquete_id == 3:
-        enquete = Enquete("","Data de nascimento: ", '2020-08-29')
-    return render(request, 'enquete.html', { "enquete" : enquete})
+    enquete = Enquete.objects.get(id=enquete_id)
+    return render(request, 'enquete.html', {"enquete": enquete})
